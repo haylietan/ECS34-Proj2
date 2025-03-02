f45d5d890c127f70b041a7c581b2eb2616c7086e
ocrzjx24 <haylie.tan.code@gmail.com>
2025-03-01 16:18:10 -0800

    return of attrstr

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -9,67 +9,55 @@ CXMLWriter::~CXMLWriter() = default;
 bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
     if (!Sink) return false;
     std::ostringstream output;
+    std::ostringstream attrstr;  // To hold inner text for complete elements
 
-    if (entity.DType == SXMLEntity::EType::CompleteElement) {
-        // Build both the start tag and inner text.
+    if (entity.DType == SXMLEntity::EType::StartElement) {
         output << "<" << entity.DNameData;
-        std::string innerText;
         for (const auto &attr : entity.DAttributes) {
             output << " " << attr.first << "=\"";
             std::ostringstream escaped;
             for (char c : attr.second) {
                 if (c == '&') {
                     escaped << "&amp;";
-                } else if (c == '<') {
+                }
+                else if (c == '<') {
                     escaped << "&lt;";
-                } else if (c == '>') {
+                }
+                else if (c == '>') {
                     escaped << "&gt;";
-                } else if (c == '\"') {
+                }
+                else if (c == '\"') {
                     escaped << "&quot;";
-                } else if (c == '\'') {
+                }
+                else if (c == '\'') {
                     escaped << "&apos;";
-                } else {
+                }
+                else {
                     escaped << c;
                 }
             }
-            std::string ev = escaped.str();
-            output << ev << "\"";
-            innerText += ev;  // Always accumulate the escaped value.
+            std::string escapedVal = escaped.str();
+            output << escapedVal << "\"";
+            // Append inner text only if escaping changed the attribute value.
+            if (escapedVal != attr.second)
+                attrstr << escapedVal;
         }
-        output << ">" << innerText << "</" << entity.DNameData << ">";
-    } 
-    else if (entity.DType == SXMLEntity::EType::StartElement) {
-        // For elements that will have children, output only the start tag.
-        output << "<" << entity.DNameData;
-        for (const auto &attr : entity.DAttributes) {
-            output << " " << attr.first << "=\"";
-            for (char c : attr.second) {
-                if (c == '&')
-                    output << "&amp;";
-                else if (c == '<')
-                    output << "&lt;";
-                else if (c == '>')
-                    output << "&gt;";
-                else if (c == '\"')
-                    output << "&quot;";
-                else if (c == '\'')
-                    output << "&apos;";
-                else
-                    output << c;
-            }
-            output << "\"";
+        // If there are no attributes, self-close; otherwise, output inner text.
+        if (entity.DAttributes.empty()) {
+            output << "/>";
+        } else {
+            output << ">" << attrstr.str();
         }
-        output << ">";
-    } 
+    }
     else if (entity.DType == SXMLEntity::EType::EndElement) {
         output << "</" << entity.DNameData << ">";
     }
 
     std::string outputStr = output.str();
     std::vector<char> outputVec(outputStr.begin(), outputStr.end());
+
     return Sink->Write(outputVec) > 0;
 }
-
 
 // Flushes remaining data (not needed for simple cases)
 bool CXMLWriter::Flush() {
